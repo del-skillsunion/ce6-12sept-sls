004e6122bbc3d7ea795affe71640ea3f58aa3534
Delfrinando Pranata <delfrinando@Delfrinandos-MacBook-Pro.local>
2024-09-13 21:38:49 +0800

    Add publish message

--- handler.js
+++ handler.js
@@ -1,11 +1,35 @@
+const { SNSClient, PublishCommand } = require("@aws-sdk/client-sns");
+
+const snsClient = new SNSClient({ region: "ap-southeast-1" });
+
 module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
+  try {
+    const eventText = JSON.stringify(event, null, 2);
+    // Define parameters
+    const params = {
+      Message: eventText,
+      Subject: "Test SNS From Lambda",
+      TopicArn: "arn:aws:sns:ap-southeast-1:255945442255:delfrinando-email-topic"
+    };
+
+    // Publish the message
+    const data = await snsClient.send(new PublishCommand(params));
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
         message: "Go Serverless v3.0! Your function executed successfully!",
-        className: process.env.CLASS_NAME
-      },
-    ),
-  };
+        className: process.env.CLASS_NAME,
+        snsResponse: data
+      }),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Error publishing message",
+        error: err.message,
+      }),
+    };
+  }
 };
